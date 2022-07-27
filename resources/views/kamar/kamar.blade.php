ce3e86139056069d3dfa81cd5dbee396bc07dd0e
zeinmukaffi <zeinau.16@gmail.com>
2022-07-27 21:45:14 +0700

    CRUD KAMAR

---
+++ resources/views/kamar/kamar.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.nav')
+@section('isi')
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <a href="{{ url('kamar/create') }}" class="btn btn-danger btn-circle btn-lg mb-3">
+                <i class="fas fa-plus"></i>
+            </a>
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Tipe Kamar</th>
+                        <th>Jumlah Kamar</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($kamar as $index => $item)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->tipe_kamar }}</td>
+                        <td>{{ $item->jumlah_kamar }}</td>
+                        <td>
+                            <form action="{{ url('kamar',$item->id) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-icon btn-danger delete" data-name="{{ $item->tipe_kamar }}"><i class="fas fa-trash"></i></button>
+                            </form>
+                            <a href="{{ url('kamar/'.$item->id.'/edit') }}" class="btn btn-icon btn-warning mt-1"><i class="fas fa-pen"></i></a>
+                            </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            
+        </div>
+    </div>
+</div>
+
+@endsection
